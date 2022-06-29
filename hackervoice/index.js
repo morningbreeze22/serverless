5603aef61243a8ad6f66738b7a245c584fc72017
Jiawei <582973876@qq.com>
2022-06-28 22:40:35 -0700

    week step 5

--- hackervoice/index.js
+++ hackervoice/index.js
@@ -2,9 +2,9 @@ module.exports = async function (context, req) {
     context.log('JavaScript HTTP trigger function processed a request.');
 
     const psw = (req.query.password || (req.body && req.body.password));
-    const responseMessage = psw
-        ? psw
-        : "This HTTP triggered function executed successfully. Pass a password in the query string or in the request body for a personalized response.";
+    const responseMessage = psw==="letmein"
+        ? "Access granted."
+        : "Access denied.";
 
     context.res = {
         // status: 200, /* Defaults to 200 */
