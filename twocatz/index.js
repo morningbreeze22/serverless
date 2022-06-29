c19d0a646136a97e5a79ab75d3fca858f853285e
Jiawei <582973876@qq.com>
2022-06-29 00:10:52 -0700

    add random name and return 2 cats

--- twocatz/index.js
+++ twocatz/index.js
@@ -1,17 +1,39 @@
 const fetch = require('node-fetch');
 
+
+
 module.exports = async function (context, req) {
     context.log('JavaScript HTTP trigger function processed a request.');
 
-    const resp = await fetch("https://bit-cat.azurewebsites.net/cat/says/serverless", {
-        method: 'GET'
-    });
-    
-    const data = await resp.arrayBuffer();
-    var base64data = Buffer.from(data).toString("base64"); 
+    let namelist = ["Shreya", "Emily", "Fifi", "Beau", "Evelyn", "Julia", "Daniel", "Fardeen"];
+
+    async function getpic(){
+        const resp = await fetch("https://bit-cat.azurewebsites.net/cat/says/serverless", {
+            method: 'GET'
+        });
+        
+        const data = await resp.arrayBuffer();
+        var base64data = Buffer.from(data).toString("base64"); 
+        return base64data;
+    }
+
+    function getRandomName(namelist){
+        let idx = Math.floor(namelist.length * Math.random());
+        let resultName = namelist[idx];
+        return resultName;
+    }
+    let name1 = getRandomName(namelist);
+    let name2 = getRandomName(namelist);
+
+    let pic1 = await getpic();
+    let pic2 = await getpic();
 
     context.res = {
         // status: 200, /* Defaults to 200 */
-        body: { base64data }
+        body: { 
+            cat1: pic1,
+            cat2: pic2,
+            names: [name1, name2]
+        }
     };
 }
